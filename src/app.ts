0c1dc157a2cacd13200f2726e796373791437c82
clew <lewis@clewup.co.uk>
2023-02-14 21:59:29 +0000

    Initialized discord.js.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,27 @@
 import express from 'express';
+import {Client, IntentsBitField as Intents } from 'discord.js'
+
+require('dotenv').config();
+
 const app = express();
 const port = 3000;
+const client = new Client({intents: [Intents.Flags.Guilds, Intents.Flags.GuildMessages]});
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
     res.send('Hello World!');
+});*/
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client?.user?.tag}!`);
+})
+
+client.login(process.env.CLIENT_TOKEN);
+
+client.on('messageCreate', msg => {
+    console.log(msg);
+    if (msg.content === 'Hello') {
+        msg.reply(`Hello ${msg.author.username}`);
+    }
 });
 
 app.listen(port, () => {
